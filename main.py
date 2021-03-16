496c7035d70c2659cdc821a9c0b0b478d89d2e55
Replit user <replituser@example.com>
2021-03-16 20:14:49 +0000

    1ºrevision

--- main.py
+++ main.py
@@ -1,58 +1,68 @@
 class Rectangle :
-  def __init__(self,widht,height) :
-    self.widht = widht
+  def __init__(self,width,height) :
+    self.width = width
     self.height = height
     
-  def set_width (self, new_widht) :
-    self.widht = new_widht
+  def set_width (self, new_width) :
+    self.width = new_width
     
   def set_height (self, new_height) :
     self.height = new_height
     
   def get_area (self) :
-    return self.widht * self.height
+    return self.width * self.height
   
   def get_perimeter (self) :
-    return 2 * self.widht + 2 * self.height
+    return 2 * self.width + 2 * self.height
   
   def get_diagonal (self) :
-    return (self.widht**2 + self.height ** 2) **.5
+    return (self.width**2 + self.height ** 2) **.5
   
   def get_picture (self) :
     #el numero de lineas a imprimir = height
     star = ''
     for i in range (self.height) :
-      if self.widht > 50 or self.height > 50 :
-        return 'Too big for picture'
+      if self.width > 50 or self.height > 50 :
+        return 'Too big for picture.'
       else :
-        star = star + '*' * self.widht + '\n'
+        star = star + '*' * self.width + '\n'
     return star
   
   def get_among_inside (self,shape) :
-    #si la clase es Cuadrado
+    #el problema está aquí
     square_side = Square.side(shape)
     print (square_side)
-    if self.widht > square_side and self.height > square_side :
-      return int ((self.widht * self.height) / square_side **2 )
+    if self.width > square_side and self.height > square_side :
+      return int ((self.width * self.height) / square_side **2 )
     else :
-      return int ((square_side ** 2) / (self.widht * self.height))
+      return int ((square_side ** 2) / (self.width * self.height))
  
 
   def __str__(self) :
-    return f"Rectangle(widht={self.widht}, height={self.height})"
+    return f"Rectangle(width={self.width}, height={self.height})"
   
   #pasamos a la segunda parte del programa
 
 class Square (Rectangle) : #comprovar si la herencia funcionaba así
   def __init__(self,lenght) : 
     self.lenght = lenght
-    self.widht = lenght
+    self.width = lenght
     self.height = lenght
 
   def set_side (self,new_lenght) :
     self.lenght = new_lenght
-    self.widht = new_lenght
+    self.width = new_lenght
     self.height = new_lenght
+  
+  def set_width (self,new_width) :
+    self.lenght = new_width
+    self.width = new_width
+    self.height = new_width
+  
+  def set_height (self,new_height) :
+    self.lenght = new_height
+    self.width = new_height
+    self.height = new_height
   
   def __str__ (self) :
     return f"Square(side={self.lenght})"
@@ -63,7 +73,7 @@ class Square (Rectangle) : #comprovar si la herencia funcionaba así
 
 rectangle1 = Rectangle (10,5)
 print (rectangle1.get_area())
-rectangle1.set_height(3)
+rectangle1.set_height(51)
 print (rectangle1)
 print (rectangle1.get_perimeter())
 print (rectangle1.get_picture())
